82cf38f40974d5b4914dc9dda93fc9b767d3ce7c
Ilya <ilyakorolev2@gmail.com>
2025-10-24 21:45:12 +0300

    update

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const port = process.env.PORT || 415; // Берём порт из Render или 
 
 // Простые данные для теста (как база; ключ — code, значение — JSON)
 const dataBase = {
-    '2021001B': { name: 'Продукт 1', description: 'Это описание для кода 2021001B', price: 100 },
-    '12345': { name: 'Продукт 2', description: 'Другое описание', price: 200 },
-    '54321': { name: 'Продукт 2', description: 'Другое описание', price: 543 },
-    '111': { name: 'Продукт 3', description: 'Ещё одно описание', price: 300 } // Твой новый элемент (я поправил name для разнообразия)
+    '202101B': { type: 'накопитель для АДМ KDS200 Standart', date: '01.01.2024', note: 100 },
+    '12345': { type: 'накопитель для АДМ KDS200 Compact', date: '01.01.2024', note: 200 },
+    '54321': { type: 'накопитель для АДМ Panda', date: '01.01.2024', note: 543 },
+    '111': { type: 'накопитель для АДМ KDS200 MAX', date: '01.01.2024', note: 300 } 
     // Добавь больше по аналогии
 };
 
 // Маршрут: GET /data/:code — получает code из URL
 app.get('/data/:code', (req, res) => {
-    const code = req.params.code; // Твой код из запроса
+    const code = req.params.code; // код из запроса
     const data = dataBase[code]; // Ищем в "базе"
     if (data) {
         res.json(data); // Возвращаем JSON
