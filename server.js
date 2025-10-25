b8929d59c1556be0c69f5331379d5f80572a3c83
Ilya <ilyakorolev2@gmail.com>
2025-10-25 15:07:00 +0300

    update

--- server.js
+++ server.js
@@ -2,33 +2,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 415; // Берём порт из Render или 415 локально
 
-// Простые данные для теста (как база; ключ — code, значение — JSON)
+// Простые данные для теста (как база; ключ - code, значение - JSON)
 const dataBase = {
     '202101B': { type: 'для АДМ KDS200 Standart', date: '01.01.2024', note: 'не была в ремонте' },
-    '12345': { type: 'для АДМ KDS200 Compact', date: '01.01.2024', note: ,'была в ремонте 01.06.2025' },
+    '12345': { type: 'для АДМ KDS200 Compact', date: '01.01.2024', note: 'была в ремонте 01.06.2025' },
     '54321': { type: 'для АДМ Panda', date: '01.01.2024', note: 'была в ремонте 06.06.2025' },
-    '11111': { type: 'для АДМ KDS200 MAX', date: '01.01.2024', note: 'была в ремонте 01.01.2025' } 
-    '25505': { type: 'накопитель для АДМ KDS200 Compact', date: '01.01.2024', note: 'была в ремонте 06.06.2025' },
-    '25501': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25502': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25503': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25504': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25505': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25506': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25507': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25508': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25509': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25510': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25511': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25512': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25513': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25514': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25515': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25516': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25517': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25518': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25519': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
-    '25520': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' ' },
+    '11111': { type: 'для АДМ KDS200 MAX', date: '01.01.2024', note: 'была в ремонте 01.01.2025' },
+    '25501': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1' },
+    '25502': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1' },
+    '25503': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' 1' },
+    '25504': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' 1' },
+    '25505': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25506': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25507': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25508': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25509': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25510': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25511': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25512': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25513': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' 1' },
+    '25514': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' 1' },
+    '25515': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: ' 1' },
+    '25516': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25517': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25518': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25519': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' },
+    '25520': { type: 'для АДМ KDS200 Compact', date: '21.10.2025', note: '1 ' }
     // Добавь больше по аналогии
 };
 
